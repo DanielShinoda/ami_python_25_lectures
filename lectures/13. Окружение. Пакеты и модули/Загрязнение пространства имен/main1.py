84b47ecec9639aa38071d63db5ba061258b46a9d
Azamatkhan Arifkhanov <60036464+Sm1Ling@users.noreply.github.com>
2025-08-23 16:13:21 +0300

    Update main1.py
    fixed typo

--- lectures/13. Окружение. Пакеты и модули/Загрязнение пространства имен/main1.py
+++ lectures/13. Окружение. Пакеты и модули/Загрязнение пространства имен/main1.py
@@ -3,9 +3,9 @@ from math_ops import *
 
 crucial_func()
 
-any_numer_seq = [1,2,2,51,2,2,1]
+any_number_seq = [1,2,2,51,2,2,1]
 
-print(max(any_numer_seq))
+print(max(any_number_seq))
 
 # Иной типичный пример, это когда делают `from numpy import *`
 # Numpy -- большая библиотека с матричными операциями. Многие её функции
